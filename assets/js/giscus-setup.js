009b9e689a29dcd759a18bb88937cbb2e94bd95a
g-s01 <95131287+g-s01@users.noreply.github.com>
2025-09-15 23:28:58 +0000

    Deploying to gh-pages from @ g-s01/g-s01.github.io@0fcfb4b30194edb28e7299fba3427948d3e5bedb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "g-s01/g-s01.github.io",
-    "data-repo-id": "",
-    "data-category": "Comments",
-    "data-category-id": "",
+    "data-repo-id": "R_kgDONKrong",
+    "data-category": "Announcements",
+    "data-category-id": "DIC_kwDONKrons4CkAT2",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
